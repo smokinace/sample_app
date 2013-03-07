faeb99d3713b255a6ddbe5c5b044f791d69bf0f8
smokinace <brakoci@gmail.com>
2013-03-07 15:27:34 -0700

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,4 +43,29 @@ describe "Static pages" do
 						:text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+	describe "About page" do
+
+			it "should have the h1 'About Us'" do
+				visit '/static_pages/about'
+				page.should have_selector('h1', :text => 'About Us')
+			end
+
+			it "should have the title 'About Us'" do
+				visit '/static_pages/about'
+				page.should have_selector('title',
+									:text => "Ruby on Rails Tutorial Sample App | About Us")
+		end
+	end
+	describe "Contact page" do
+
+		it "should have the h1 'contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+	end
+	
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('title',	:text => "Ruby on Rails Tutorial Sample App | Contact")
+		end
+	end
 end
